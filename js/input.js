31027eb9ac762cf20ccd8b6cbdd1dcdcf83d22b2
Cahit <103262027+cahitdogan@users.noreply.github.com>
2023-09-22 17:14:10 +0300

    Add files via upload

--- js/input.js
+++ js/input.js
@@ -4,3 +4,8 @@ const operationScreen = document.querySelector("input[type='text']");
 operationScreen.addEventListener("input", function() {
     operationScreen.value = operationScreen.value.replace(/[^0-9+\-×÷()%.,]/g, "");
 });
+
+operationScreen.addEventListener("click", function(event) {
+    event.preventDefault();
+    this.focus();
+});

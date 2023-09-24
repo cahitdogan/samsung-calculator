41b73ba9696d651cd45a05c69fca6df4e554e180
Cahit <103262027+cahitdogan@users.noreply.github.com>
2023-09-24 09:21:04 +0300

    Add files via upload

--- js/input.js
+++ js/input.js
@@ -4,3 +4,9 @@ const operationScreen = document.querySelector("#operation-screen");
 operationScreen.addEventListener("input", function() {
     operationScreen.value = operationScreen.value.replace(/[^0-9+\-×÷()%.,]/g, "");
 });
+
+const deleteButton = document.querySelector("#delete");
+
+deleteButton.addEventListener("onMouseDown", function(event) {
+    event.preventDefault();
+})

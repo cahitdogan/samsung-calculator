287f8b78e7508c4adc9c6b0a694142ea3e873fdc
Cahit <103262027+cahitdogan@users.noreply.github.com>
2023-10-09 19:27:44 +0300

    Add files via upload

--- js/delete.js
+++ js/delete.js
@@ -7,42 +7,51 @@ operationScreen.addEventListener("input", function() {
     /*.value property returns "" when the input has not any text.
      "this" blinds to listening object when using in a event handler*/
      if ((this.value !== "") && (deleteButtonState === false)) { 
-        deleteButton.classList.add("active"); // Q: why not work? A:  
-        deleteButton.style.backgroundImage = "url(images/delete-active.svg)";
+        deleteButton.classList.add("active");
+        deleteButton.style.opacity = "1";
+        deleteButton.style.cursor = "pointer";
         deleteButtonState = true;
     } else if ((this.value === "") && (deleteButtonState === true)) { 
         deleteButton.classList.remove("active");
-        deleteButton.style.backgroundImage = "url(images/delete.svg)";
+        deleteButton.style.opacity = "0.4";
+        deleteButton.style.cursor = "default";
         deleteButtonState = false;
     }
 }); 
 
+
 /*behaviours of the delete button*/
-deleteButton.addEventListener("click", function() {
-        navigator.vibrate(50);
+deleteButton.addEventListener("click", function() { //this works for one charatcher or selection delete. you need to find another solution so that it continues deleting even when you press and hold it.
+    if (deleteButtonState === true) {
 
-    let caretPosition = operationScreen.selectionStart; //returns position of the caret and positions of selections
+        navigator.vibrate(65);
+        
+        let caretPosition = operationScreen.selectionStart; //returns position of the caret and start position of the selection
 
-    if ((operationScreen.selectionStart - operationScreen.selectionEnd) === 0) { //delete and focus functions for the caret
-        if (caretPosition > 0) {
-            operationScreen.value = operationScreen.value.substring(0, caretPosition - 1) +
-                                    operationScreen.value.substring(caretPosition, 200);
-            operationScreen.setSelectionRange(caretPosition - 1, caretPosition - 1);
-            operationScreen.focus();
-        } else if (caretPosition === 0) { 
-            operationScreen.setSelectionRange(0, 0);
+        if ((operationScreen.selectionStart - operationScreen.selectionEnd) === 0) { //delete and focus functions for the caret
+            if (caretPosition > 0) {
+                operationScreen.value = operationScreen.value.substring(0, caretPosition - 1) +
+                                        operationScreen.value.substring(caretPosition, 200);
+                operationScreen.setSelectionRange(caretPosition - 1, caretPosition - 1);
+                operationScreen.focus();
+            } else if (caretPosition === 0) { 
+                operationScreen.focus();
+            }
+        } else { //delete and focus functions for the selection
+            operationScreen.value = operationScreen.value.substring(0, operationScreen.selectionStart) +
+                                    operationScreen.value.substring(operationScreen.selectionEnd, operationScreen.value.lenght);
+            operationScreen.setSelectionRange(caretPosition, caretPosition);
             operationScreen.focus();
         }
-    } else { //delete and focus functions for the selection
-        operationScreen.value = operationScreen.value.substring(0, operationScreen.selectionStart) +
-                                operationScreen.value.substring(operationScreen.selectionEnd, operationScreen.value.lenght);
-        operationScreen.setSelectionRange(caretPosition, caretPosition);
-        operationScreen.focus();
-    }
 
-    if ((operationScreen.value === "") && (deleteButtonState === true)) { 
-        deleteButton.classList.remove("active");
-        deleteButton.style.backgroundImage = "url(images/delete.svg)";
-        deleteButtonState = false;
+        if (operationScreen.value === "") { 
+            deleteButton.classList.remove("active");
+            deleteButton.style.opacity = "0.4";
+            deleteButton.style.cursor = "default";
+            deleteButtonState = false;
+        }
     }
 });
+
+
+//click eventinin değiştirilmesi haricinde bu kod üzerinde değiştirilmesi gereken başka bir şey yok.
---
+++ js/focus.js
@@ -0,0 +1,5 @@
+const body = document.querySelector("body");
+
+body.addEventListener("click", function(){
+    operationScreen.focus();
+});
